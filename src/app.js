f4d0ea93cc65a903ba2dd3cd9252993303655242
sagarkataria <sagarsingh0110@gmail.com>
2024-10-01 01:21:45 +0530

    make first api of signup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 const express = require('express');
-require('./config/database')
+const connectDB = require('./config/database')
+const User = require("./models/user")
 const app = express();
 
+app.post('/signup',async (req,res)=>{
+    
+    const user = new User({
+        firstName : "Virat",
+        lastName : "Singh",
+        emailId : "sagar@gmail.com",
+        password : "sagar123"
+    });
+   await user.save(); 
+   res.send("User saved successfully")
+})
 
-app.listen(3000,()=>{
-    console.log(`Server successfully listening on port http://localhost:${3000}`);
-});
+connectDB().then(() => {
+    console.log("Connected to MongoDB")
+    app.listen(7777, () => {
+        console.log(`Server successfully listening on port http://localhost:${7777}`);
+    });
+}).catch((err) => {
+    console.log(err.message)
+})
+
+
 
--- src/config/database.js
+++ src/config/database.js
@@ -5,9 +5,6 @@ const connectDB = async () => {
         "mongodb+srv://sk-nodejs:sk-nodejs@cluster0.uq6lmlt.mongodb.net/sk-nodejs"
     )
 }
+module.exports = connectDB;
 
-connectDB().then(()=>{
-    console.log("Connected to MongoDB")
-}).catch((err)=>{
-    console.log(err.message)
-})
+
