34c42be15ba41aba64647e9ed8cc2409813e4c50
sagarkataria <sagarsingh0110@gmail.com>
2024-10-02 11:47:15 +0530

    get apis

--- src/app.js
+++ src/app.js
@@ -15,6 +15,47 @@ app.post('/signup',async (req,res)=>{
     }
 })
 
+app.get('/user',async (req,res)=>{
+    const userEmail = req.body.emailId;
+
+    try {
+        const user = await User.findOne({emailId:userEmail});
+        if(!user){
+            res.status(404).send('User not found');
+        }else{
+            res.send(user);
+        }
+    } catch (error) {
+        res.status(400).send("Something went wrong");
+    }
+
+    // try {
+    //    const user =  await User.find({'emailId':userEmail});
+    //    if(user.length===0){
+    //      res.status(404).send("User not found");
+    //    }else{
+    //      res.send(user);
+    //    }
+    // } catch (error) {
+    //     res.status(400).send("Something went wrong");
+    // }
+});
+
+
+
+app.get('/feed',async(req,res)=>{
+    try {
+        const users = await User.find({});
+        if(users.length===0){
+            res.status(404).send("No users found");
+        }else{
+            res.send(users);
+        }
+    } catch (error) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
 connectDB().then(() => {
     console.log("Connected to MongoDB")
     app.listen(7777, () => {
