3d8817be0cd3cebf2c272156fa55f95dc8c31418
sagarkataria <sagarsingh0110@gmail.com>
2024-10-02 22:43:06 +0530

    use bcrypt password

--- src/app.js
+++ src/app.js
@@ -3,26 +3,39 @@ const connectDB = require('./config/database')
 const User = require("./models/user")
 const app = express();
 
+const { validateSignUpData } = require('./utils/validation')
+const bcrypt = require('bcrypt');
+
 app.use(express.json());
 
-app.post('/signup',async (req,res)=>{
-    const user = new User(req.body);
+app.post('/signup', async (req, res) => {
     try {
-        await user.save(); 
+        validateSignUpData(req);
+        const { firstName,lastName, emailId, password } = req.body;
+
+        const passwordHash = await bcrypt.hash(password,10);
+
+        const user = new User({
+            firstName,  
+            lastName,
+            emailId,
+            password:passwordHash
+        });
+        await user.save();
         res.send("User saved successfully")
     } catch (error) {
-        res.status(400).send("Error saving the user: " + error.message)
+        res.status(400).send("ERROR : " + error.message)
     }
 })
- 
-app.get('/user',async (req,res)=>{
+
+app.get('/user', async (req, res) => {
     const userEmail = req.body.emailId;
 
     try {
-        const user = await User.findOne({emailId:userEmail});
-        if(!user){
+        const user = await User.findOne({ emailId: userEmail });
+        if (!user) {
             res.status(404).send('User not found');
-        }else{
+        } else {
             res.send(user);
         }
     } catch (error) {
@@ -41,7 +54,7 @@ app.get('/user',async (req,res)=>{
     // }
 });
 
-app.delete('/user',async(req,res)=>{
+app.delete('/user', async (req, res) => {
     const userId = req.body.userId;
     try {
         const user = await User.findByIdAndDelete(userId);
@@ -51,32 +64,32 @@ app.delete('/user',async(req,res)=>{
     }
 })
 // Update the user 
-app.patch('/user',async(req,res)=>{
-     const userId = req.body.userId;
-     const data   = req.body;
-     try {
+app.patch('/user', async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
 
-        const ALLOWED_UPDATES = ["photoUrl","about","gender","age","skillls"]; 
-        const isUpdateAllow = Object.keys(data).every((k)=>ALLOWED_UPDATES.includes(k));
+        const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skillls"];
+        const isUpdateAllow = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
 
-        if(!isUpdateAllow){
-           throw new Error("Update not Allow");
+        if (!isUpdateAllow) {
+            throw new Error("Update not Allow");
         }
-        
 
-        const user = await User.findByIdAndUpdate({_id:userId},data);
+
+        const user = await User.findByIdAndUpdate({ _id: userId }, data);
         res.send("User updated successfully");
-     } catch (error) {
+    } catch (error) {
         res.status(400).send("Something went wrong ");
-     }
+    }
 })
 
-app.get('/feed',async(req,res)=>{
+app.get('/feed', async (req, res) => {
     try {
         const users = await User.find({});
-        if(users.length===0){
+        if (users.length === 0) {
             res.status(404).send("No users found");
-        }else{
+        } else {
             res.send(users);
         }
     } catch (error) {
